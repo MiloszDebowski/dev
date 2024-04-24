f1a2d80bbc865eebefab64a6d41b7fecc833d16b
milosz <14milosz@gmail.com>
2024-04-24 12:53:46 +0200

    Wordle Mini Project

--- Web/Wordle Project/script.js
+++ Web/Wordle Project/script.js
@@ -1,7 +1,9 @@
 const ROUNDS=6;
 const ANSWER_LENGTH=5;
 const letters = document.querySelectorAll(".letter-box");
+const msg=document.querySelector(".msg");
 
+const API="https://words.dev-apis.com/word-of-the-day";
 function isLetter(letter)
 {
     return /^[a-zA-Z]$/.test(letter);
@@ -13,13 +15,21 @@ function isLetter(letter)
 
 
 
-function init()
+async function init()
 {
 
     let currentRow=0;
     let currentGuess="";
     let done=false;
     let loading=true;
+    
+    const result=await fetch(API);
+    const {word: wordRes} = await result.json();
+    const correctWord=wordRes.toUpperCase(); 
+    const correctWordLetters=correctWord.split("");//tworzy tablice liter ze słowa
+
+    
+
 
     function typeLetter(letter)
     {
@@ -36,44 +46,89 @@ function init()
     {
         if(currentGuess!=="")
         {
-            letters[currentRow * ANSWER_LENGTH + currentGuess.length -1].innerText=letter;
+            currentGuess=currentGuess.substring(0,currentGuess.length-1);
+            letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText="";
         }
     }
 
-    function enter()
+    
+    async function enter()
     {
         if(currentGuess.length<ANSWER_LENGTH)
         {}
         else
         {
+            const res = await fetch("https://words.dev-apis.com/validate-word", {
+            method: "POST",
+            body: JSON.stringify({ word: currentGuess }),
+            });
+            const { validWord } = await res.json();
+            
+            if (!validWord) 
+            {
+                return;
+            }
+
+    
+            currentGuess=currentGuess.toUpperCase();
+            const guessParts=currentGuess.split("");
+            let allRight=true;
+            
+            for(let i=0;i<ANSWER_LENGTH;i++)
+            {
+                if(guessParts[i]===correctWordLetters[i])
+                {
+                    letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+                }
+                else
+                {
+                    allRight=false;
+                    letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+                    for(let j=0;j<ANSWER_LENGTH;j++)
+                    {
+                        if(guessParts[i]===correctWordLetters[j] && j!==i)
+                        {
+                            letters[currentRow * ANSWER_LENGTH + i].classList.replace('wrong','close');
+                            break;
+                        }
+                    }
+                }
+            }
             currentRow++;
             currentGuess="";
+
+            if(allRight)
+            {
+                msg.innerText="you win"
+            }
+            else if(currentRow===ROUNDS)
+            {
+                msg.innerText=`you lose\n the word was: ${correctWord}`
+            }
         }
     }
 
     document.addEventListener("keydown", function handleKeyPress(event) {
-
+        if(done || loading)
+        {
+            return;
+        }
         const action = event.key;
 
         if (action === "Enter") 
         {
-          console.log("enter");
           enter();
         } 
         else if (action === "Backspace") 
         {
-            console.log("back")
             backspace();
         } 
         else if (isLetter(action))
         {
-            console.log(action);
             typeLetter(action);
-        
         }
         else {}
     });
 }
-
 
 init();
