5d542c7ba9aa040e95f738a362195f4911036036
Milosz <14milosz@gmail.com>
2024-04-25 13:21:31 +0200

    Metody Numeryczne

--- C++/Całkowanie numeryczne - metody geometryczne/Całkowanie numeryczne - metody geometryczne/Źródło.cpp
+++ C++/Całkowanie numeryczne - metody geometryczne/Całkowanie numeryczne - metody geometryczne/Źródło.cpp
@@ -1,52 +1,80 @@
-#include<iostream>
-#include<vector>
-#include<fstream>
-#include<sstream>
-#include<string>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
+// Funkcja podca³kowa: sin(x)
+double func1(double x) {
+    return sin(x);
+}
 
-class rectangle
-{
-private:
-	double height,base;
-public:
-	void setHeight(double height0){ height = height0; }
-	double getHeight() { return height; }
-	double getBase() { return base; }
-	void setBase(double base0){ base = base0; }
-};
+// Funkcja podca³kowa: x^2 + 2x + 5
+double func2(double x) {
+    return x * x + 2 * x + 5;
+}
 
+// Funkcja podca³kowa: exp(x)
+double func3(double x) {
+    return exp(x);
+}
 
+// Metoda prostok¹tów
+double rectangle_method(double (*func)(double), double a, double b, int n) {
+    double S = (b - a) / n;
+    double sum = 0.0;
+    for (int i = 0; i < n; ++i) {
+        double h = a + i * S + S / 2;
+        sum += func(h);
+    }
+    return sum * S;
+}
 
+// Metoda trapezów
+double trapezoidal_method(double (*func)(double), double a, double b, int n) {
+    double h = (b - a) / n;
+    double sum = (func(a) + func(b)) / 2;
+    for (int i = 1; i < n; ++i) {
+        double x_i = a + i * h;//boki
+        sum += func(x_i);
+    }
+    return sum * h;
+}
 
+// Metoda parabol
+double parabolic_method(double (*func)(double), double a, double b, int n) {
+    double S = (b - a) / n;
+    double sum = (func(a) + func(b)) / 2;
+    for (int i = 1; i < n; ++i) {
+        double x_i = a + i * S;
+        sum += 2 * func(x_i);
+    }
+    return sum * S / 2;
+}
 
+int main() {
+    double a = 0.5, b = 2.5; // Przedzia³ ca³kowania
+    int n = 4; // Liczba prostok¹tów/trapezów
 
-int main()
-{
-	int figureCount = 4;
-	rectangle* Rectangles = new rectangle[figureCount];
-	double a = 0.5;
-	double b = 2.5;
-	for (int i = 0; i < figureCount; i++)
-	{
-		Rectangles[i].setBase((b-a)/figureCount);
-		cout << endl << "Base:" << Rectangles[i].getBase() << endl;
-		Rectangles[i].setHeight(sin((Rectangles[i].getBase()*(i+1))+(Rectangles[i].getBase()/2)));
-		cout << endl << "Height:" << Rectangles[i].getHeight() << endl;
-	}
-	
-	double solution;
-	double sum = 0.0;
-	for (int i = 0; i < figureCount; i++)
-	{
-		sum += Rectangles[i].getHeight();
-	}
-	solution = sum * Rectangles[0].getBase();
-	cout << solution;
-	
-	delete[] Rectangles;
-	return 0;
+    // Ca³ka z sin(x)
+    cout << "Calka z sin(x) w przedziale od " << a << " do " << b <<". Liczba przedzialow: "<<n<< endl;
+    cout << "Metoda prostokatow: " << rectangle_method(func1, a, b, n) << endl;
+    cout << "Metoda trapezow: " << trapezoidal_method(func1, a, b, n) << endl;
+    cout << "Metoda parabol: " << parabolic_method(func1, a, b, n) << endl;
+    cout << endl;
+
+    // Ca³ka z x^2 + 2x + 5
+    b = 5.0;
+    cout << "Calka z x^2 + 2x + 5 w przedziale od " << a << " do " << b << ". Liczba przedzialow: " << n << endl;
+    cout << "Metoda prostokatow: " << rectangle_method(func2, a, b, n) << endl;
+    cout << "Metoda trapezow: " << trapezoidal_method(func2, a, b, n) << endl;
+    cout << "Metoda parabol: " << parabolic_method(func2, a, b, n) << endl;
+    cout << endl;
+
+    // Ca³ka z exp(x)
+    cout << "Calka z exp(x) w przedziale od " << a << " do " << b << ". Liczba przedzialow: " << n << endl;
+    cout << "Metoda prostokatow: " << rectangle_method(func3, a, b, n) << endl;
+    cout << "Metoda trapezow: " << trapezoidal_method(func3, a, b, n) << endl;
+    cout << "Metoda parabol: " << parabolic_method(func3, a, b, n) << endl;
+
+    return 0;
 }
