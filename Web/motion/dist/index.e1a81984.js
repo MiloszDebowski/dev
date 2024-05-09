dbad09d38ebe8b90530b3275fe95a36d96c4455b
MiloszDebowski <147827257+MiloszDebowski@users.noreply.github.com>
2024-05-09 22:01:35 +0200

    Delete Web/motion directory

--- Web/motion/dist/index.e1a81984.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".ball");popmotion.animate({from:"0px",to:"20px",repeat:1/0,repeatType:"reverse",type:"spring",onUpdate(o){console.log(o),e.style.top=o}});
-//# sourceMappingURL=index.e1a81984.js.map
