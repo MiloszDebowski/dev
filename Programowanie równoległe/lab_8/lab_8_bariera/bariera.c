c344cdf7128ae66fd6393e194da660c065de2922
MiloszDebowski <14milosz@gmail.com>
2024-11-25 12:29:54 +0100

    Update bariera.c

--- Programowanie równoległe/lab_8/lab_8_bariera/bariera.c
+++ Programowanie równoległe/lab_8/lab_8_bariera/bariera.c
@@ -3,8 +3,8 @@
 #include <pthread.h>
 
 // Zmienne statyczne
-static int ilosc_watkow = 0;                // Liczba wątków
-static int ilosc_watkow_w_funkcji = 0;      // Liczba wątków na barierze
+static int ilosc_watkow = 0;
+static int ilosc_watkow_w_funkcji = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; 
 pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
 
